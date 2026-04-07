79b81382273aa63d202d0b6477a72c05dfe08877
Sargunjot-Kaur <sargunxsandhu@icloud.com>
2026-04-08 00:50:29 +0200

    Updated queries

--- PICEducationdb-Schema.sql
+++ PICEducationdb-Schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE PICEducationdb;
 USE PICEducationdb;
 
--- Stores all registered students
+-- 1. student table: stores data of all students who have registered
 CREATE TABLE student(
     student_id  INT PRIMARY KEY AUTO_INCREMENT,
 	student_name VARCHAR(30) NOT NULL, 
@@ -9,44 +9,44 @@ CREATE TABLE student(
     student_number VARCHAR(12), 
     registration_date DATETIME);
 
--- Stores tests (e.g., "PTE Academic")
+-- 2. test table: Stores the name of test ("PTE Academic"/"PTE General")
 CREATE TABLE test(
     test_id INT PRIMARY KEY AUTO_INCREMENT, 
     test_name VARCHAR(30) NOT NULL, 
-    test_marks INT, 
-    duration TIME);
+    test_marks INT, -- total mark weightage of the test
+    duration TIME); -- time allocated for the test
 
--- Stores each time a student attempts a test
+-- 3. attempt table: Stores the number of times a student attempts a test
 CREATE TABLE attempt(
     attempt_id INT PRIMARY KEY AUTO_INCREMENT, 
     student_id INT, 
-    test_id INT, 
+    test_id INT, -- refers to name of the test taken
     attempt_date DATETIME, 
     FOREIGN KEY (student_id) REFERENCES student(student_id),
     FOREIGN KEY (test_id) REFERENCES test(test_id));
     
--- Each test has multiple sections (Reading, Writing, Listening, Speaking)
+-- 4. section table: Each test has 4 sections (Reading, Writing, Listening, Speaking)
 CREATE TABLE section(
     section_id INT PRIMARY KEY AUTO_INCREMENT,
     test_id INT,
     section_name VARCHAR(30),
     FOREIGN KEY (test_id) REFERENCES test(test_id));
     
--- Stores questions under each section    
+-- 5. question table: Stores questions under each section    
 CREATE TABLE question(
     question_id INT PRIMARY KEY AUTO_INCREMENT,
-    section_id INT,
+    section_id INT, -- refers which section the question belongs to
     correct_answer TEXT NOT NULL, -- the correct answer to each question
     question_text TEXT NOT NULL,
     marks INT,
     FOREIGN KEY (section_id) REFERENCES section(section_id));
     
--- Stores answers given by student in an attempt    
+-- 6. answer table: Stores answers given by student in a particular attempt    
 CREATE TABLE answer(
     answer_id INT PRIMARY KEY AUTO_INCREMENT,
     question_id INT,
     attempt_id INT,
-    student_answer TEXT,
+    student_answer TEXT, -- answer given by the student
     marks_awarded INT NOT NULL, -- marks given per answer
     FOREIGN KEY (question_id) REFERENCES question(question_id),
     FOREIGN KEY (attempt_id) REFERENCES attempt(attempt_id));
